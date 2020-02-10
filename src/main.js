bfc4cdf1c90d9d2cced0eea97cbcd5430c3fd17a
Emanuellukas <lucasesn23@gmail.com>
2020-02-10 01:16:52 -0300

    Início da aplicação com ESC6+

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import * as functions from './scripts/calculadora';
+/*import * as functions from './scripts/calculadora';
 
 console.log(functions.soma(5, 1, 4))
 //10
@@ -8,5 +8,75 @@ console.log(functions.sub(5, 1, 4))
 
 console.log(functions.mult(5, 2, 4))
 //40
+*/
 
-alert('alou')
+import api from './api'
+
+class App {
+    constructor (){
+        this.repositories = [];
+
+        this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repository]');
+        this.listEl = document.getElementById('repo-list');
+
+        this.registerHandlers();
+    }
+
+    registerHandlers(){
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    async addRepository(event){
+        event.preventDefault();
+
+        const repoInput = this.inputEl.value;
+
+        if(repoInput.lenght === 0)
+            return;
+
+        const response = await api.get(`/repos/${repoInput}`);
+
+        console.log(res)
+
+        this.repositories.push({
+            name: 'rocketseat.com.br',
+            description: 'Tire a sua ideia do papel e dê vida à sua startup.',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+            html_url: 'https://github.com/rocketseat/rocketseat.com.br'
+        });
+
+        console.log(this.repositories);
+
+        this.render();
+    }
+
+    render(){
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p')
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a')
+            linkEl.setAttribute('target', '_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl)
+        });
+    }
+}
+
+new App();
